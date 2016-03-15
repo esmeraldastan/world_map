da908dc261e1899874f74537253dbf7c12b2177e
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-03-15 11:37:14 -0700

    office
    part of the world map

---
+++ World map (office).py
@@ -0,0 +1,41 @@
+import sys
+
+node = None 
+
+class building:
+    
+    def __init__(self, name, description, up, down, east, west, north, south, left, right):
+        self.name = name
+        self.description = description
+        self.up = up
+        self.down = down
+        self.east = east
+        self.west = west
+        self.north = north
+        self.south = south
+        self.left = left
+        self.right =right
+        
+        def move(self, direction):
+            global node 
+            node = globals()[getattr(self, direction)]
+            
+            
+main_office = building('Office', 'Papers have been shattered.', None, None, None, None, None,None, None, None)
+elevator = building('', '', None, None, None, None, None,None, None, None)
+room1 = building('', '', None, None, None, None, None,None, None, None)
+stairs  = building ('', '', None, None, None, None, None,None, None, None)
+meeting_room = building('', '', None, None, None, None, None,None, None, None)
+secutary_office = building('', '', None, None, None, None, None,None, None, None)
+weapon_room = building('', '', None, None, None, None, None,None, None, None)
+ganiter_room = building('', '', None, None, None, None, None,None, None, None)
+bathroom = building('', '', None, None, None, None, None,None, None, None)
+secret_door = building ('', '', None, None, None, None, None,None, None, None)
+
+
+node = main_office 
+
+command = raw_input('>').strip().lower()
+if command in ['q', 'exit', 'quit']:
+    sys.exit(0)
+
