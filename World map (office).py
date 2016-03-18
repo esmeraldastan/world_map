f8cc8804615e4812b68deca22e8bfedd21e356ef
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-03-18 11:54:13 -0700

    3/18/16
    map half way done

--- World map (office).py
+++ World map (office).py
@@ -1,8 +1,13 @@
 import sys
 
+#Name of the Game 
+print "Welcome to CounterCode"
+
+
+
 node = None 
 
-class building:
+class building:#starting of the game 
     
     def __init__(self, name, description, up, down, east, west, north, south, left, right):
         self.name = name
@@ -20,17 +25,31 @@ class building:
             global node 
             node = globals()[getattr(self, direction)]
             
-            
-main_office = building('Office', 'Papers have been shattered.', None, None, None, None, None,None, None, None)
-elevator = building('', '', None, None, None, None, None,None, None, None)
-room1 = building('', '', None, None, None, None, None,None, None, None)
-stairs  = building ('', '', None, None, None, None, None,None, None, None)
-meeting_room = building('', '', None, None, None, None, None,None, None, None)
-secutary_office = building('', '', None, None, None, None, None,None, None, None)
-weapon_room = building('', '', None, None, None, None, None,None, None, None)
-ganiter_room = building('', '', None, None, None, None, None,None, None, None)
-bathroom = building('', '', None, None, None, None, None,None, None, None)
-secret_door = building ('', '', None, None, None, None, None,None, None, None)
+#Building
+#first floor                        
+main_office = building('Office', 'Papers have been shattered.', None, None,'Secutary Desk', "Meeting Room", None,None, None, None)
+elevator = building('Elevator', '', None, None, None, None, None,None, None, None)
+secutary_office = building('Secutary Desk', '', None, None, None, None, None,None, None, None)
+stairs  = building ('Stairs', '', None, None, None, None, None,None, None, None)
+meeting_room = building('Meeting Room', '', None, None, 'Elevator', None, None, 'Office', None, None)
+
+#second floor
+weaponary_room = building('Secret Room', '', None, None, None, None, None,None, None, None)
+ganitor_room = building(' ', '', None, None, None, None, None,None, None, None)
+office1 = building('', '', None, None, None, None, None,None, None, None)
+office2 = building('', '', None, None, None, None, None,None, None, None)
+office3= building ('', '', None, None, None, None, None,None, None, None)
+bathroom = building ('', '', None, None, None, None, None,None, None, None)
+
+#first floor
+
+#city
+bank = building ('Bank', '', None, None, None, None, None,None, None, None)
+coffee_shop = building ('Coffee Shop', '', None, None, None, None, None,None, None, None)
+building2 = building ('Insurence Building', '', None, None, None, None, None,None, None, None)
+drug_store = building ('Pharmacy ', '', None, None, None, None, None,None, None, None)
+
+
 
 
 node = main_office 
