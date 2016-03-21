c273bed7a34dd6dbe7fb89da1d1a178bd5028a30
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-03-20 23:33:45 -0700

    3/20/16
    fill up description

--- World map (office).py
+++ World map (office).py
@@ -2,9 +2,9 @@ import sys
 
 node = None 
 
-class building:
+class Building:
     
-    def __init__(self, name, description, up, down, north, east, south, west, right, left, outside, inside ):
+    def __init__(self, name, description, up, down, north, east, south, west, right, left, outside, inside):
         self.name = name
         self.description = description
         self.up = up
@@ -18,25 +18,25 @@ class building:
         self.outside = outside 
         self.inside = inside 
         
-        def move(self, direction):
-            global node 
-            node = globals()[getattr(self, direction)]
+    def move(self, direction):
+        global node 
+        node = globals()[getattr(self, direction)]
             
 #BUILDING
 #THIRD FLOOR            
-main_office = building('Office', 'Papers have been shattered.', None, None, 'Conference Room', 'Secutary Desk', None, None, None, None, None, None)
-elevator = building('Elevator', '', None, None, None, None, 'Secutary Desk',None, None, None, None, None)
-meeting_room = building('Conference Room', '', None, None, None, 'Elevator', 'Secutary Desk',None, None, None, None, None)
-stairs  = building ('Stairs', '', None, None, None, None, None,None, None, None, None, None)
-secutary_office = building('Secutary Desk', '', None, None, None, None, None , None, None, None, None, None)
+Office = Building("Office", 'Papers have been shattered.', None, None, 'Conference', 'Secutary', None, None, None, None, None, None)
+Conference = Building("Conference Room", 'tables are shattered every where ', None, None, None, 'Elevator', 'Office',None, None, None, None, None)
+Elevator = Building("Elevator", '', None, None, None, None, 'Secutary Desk',None, None, None, None, None)
+Stairs = Building("Stairs", '', None, None, None, None, None,None, None, None, None, None)
+Secutary = Building("Secutary Desk", '', None, None, 'Elevator', None, None , None, None, None, None, None)
 
-#SECOND FLOOR
+'''#SECOND FLOOR
 weapon_room = building('', '', None, None, None, None, None,None, None, None, None, None)
 secret_door = building('', '', None, None, None, None, None,None, None, None, None, None)
 ganiter_room = building('', '', None, None, None, None, None,None, None, None, None, None)
 bathroom = building('', '', None, None, None, None, None,None, None, None, None, None)
-room1 = building ('', '', None, None, None, None, None,None, None, None, None, None)
-room2 = building ('', '', None, None, None, None, None,None, None, None, None, None)
+room1 = Building('', '', None, None, None, None, None,None, None, None, None, None)
+room2 = Building('', '', None, None, None, None, None,None, None, None, None, None)
 
 #FIRST FLOOR
 
@@ -45,8 +45,8 @@ room2 = building ('', '', None, None, None, None, None,None, None, None, None, N
 insurence_building=('', '', None, None, None, None, None,None, None, None, None, None)
 coffee_shop = ('', '', None, None, None, None, None,None, None, None, None, None)
 bank =('', '', None, None, None, None, None,None, None, None, None, None)
-
-node = main_office 
+'''
+node = Office
 
 #Run program 
 
@@ -57,7 +57,6 @@ while True:
     print "Description: " + node.description 
     
     response = ['up', 'down', 'north', 'east', 'south', 'west', 'right', 'left', 'outside', 'inside'] 
-    
     command = raw_input('>').strip().lower()
     #QUITE THE PROGRAM 
     if command in ['q', 'exit', 'quit']:
