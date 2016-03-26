015e8086037dbdd5d0dca34c402f24c5fc44533a
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-03-25 22:54:14 -0700

    half done

--- World map (office).py
+++ World map (office).py
@@ -30,22 +30,23 @@ Stairs = Building("Stairs", '', None, 'Stairs1', None, None, None,None, None, No
 Secutary = Building("Secutary Desk", '', None, None, 'Elevator', None, None , None, None, None, None, None)
 
 Stairs1 = Building("Stairs", '', None, None, None, None, None,None, None, None, None, None)
-'''#SECOND FLOOR
-weapon_room = building('', '', None, None, None, None, None,None, None, None, None, None)
-secret_door = building('', '', None, None, None, None, None,None, None, None, None, None)
-ganiter_room = building('', '', None, None, None, None, None,None, None, None, None, None)
-bathroom = building('', '', None, None, None, None, None,None, None, None, None, None)
-room1 = Building('', '', None, None, None, None, None,None, None, None, None, None)
-room2 = Building('', '', None, None, None, None, None,None, None, None, None, None)
+#SECOND FLOOR
+Weapon = Building('Weapon Room', '', None, None, None, None, 'Secret',None, None, None, None, None)
+Secret = Building('Secret Door', '', None, None, 'Weapon', 'Janitor', None,None, None, None, None, None)
+Janitor= Building('Janitor Room', '', None, None, None, 'Office1', 'Secret',None, None, None, None, None)
+Bathroom = Building('Restroom', '', None, None, None, None, 'Stairs','Elevator', None, None, None, None)
+Office1 = Building('Office 1', '', None, None, 'Elevator', 'Office2', 'Janitor',None, None, None, None, None)
+Office2 = Building('Office 2 ', '', None, None, None, 'Elevator', 'Stairs','Office1', None, None, None, None)
 
 #FIRST FLOOR
-
-
+Front = Building('', '', None, None, None, None, None,None, None, None, None, None) 
+'''Door = Building('', '', None, None, None, None, None,None, None, None, None, None)
+ Building('', '', None, None, None, None, None,None, None, None, None, None)
 #OUTSIDE 
 insurence_building=('', '', None, None, None, None, None,None, None, None, None, None)
 coffee_shop = ('', '', None, None, None, None, None,None, None, None, None, None)
-bank =('', '', None, None, None, None, None,None, None, None, None, None)
-'''
+bank =('', '', None, None, None, None, None,None, None, None, None, None)'''
+
 node = Office
 
 #Run program 
