15e1b6c72e6e5d354c632e65d0ae126fcf7cf630
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-02 18:24:41 -0700

    first floor

--- World map (office).py
+++ World map (office).py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 
 #Name of the Game 
@@ -36,9 +37,10 @@ print 'You have woken up form a long sleep. The last thing you remember was esca
 Office = Building("Office", 'Papers have been shattered everywhere. The lights a\nflashing on and off. There next to you is a light blue paper. Type "pick up" to read what it says.', None, None, 'Conference', 'Secutary', None, None, None, None, None, None)
 Conference = Building("Conference Room", 'You are now standing in the Conference Room. A couple of bodies are laying around. Rottening with a nasty smell. There\'s a flashlight on the table. Pick it up...you might need it later on .', None, None, None, 'Elevator', 'Office',None, None, None, None, None)
 Elevator = Building("Elevator", 'In the Elevator head down to continue getting to your destination. Type "down".But wait before that you you need to restore to full health.There is a green cyrum laying on the grown. Type "restore" this will get you to full health. ', None, 'Elevator2', None, None, 'Secutary Desk',None, None, None, None, None)
-Stairs = Building("Stairs", 'The walls are coverd with blood. You are not alone. Zombies and infecteds run the area now. You don\'t want to encounter with one ...it can be your end. ', None, 'Stairs1', None, None, None,None, None, None, None, None)
-Secutary = Building("Secutary Desk", '', None, None, 'Elevator', None, None , None, None, None, None, None)
+Stairs = Building("Stairs", 'The walls are coverd with blood. You are not alone. Zombies and infecteds run the area now. You don\'t want to encounter with one ...it can be your end.To go down the stairs type down to go on to the next floor.There is blood covering the walls….Bodies laying down with body parts missing. Be careful.  ', None, 'Stairs1', None, None, None,None, None, None, None, None)
+Secutary = Building("Secutary Desk",' You are standing next to your securary\'s desk. A flash light stands on top. Pick it up you might need it later on. Head "north" to the elevator or "east" to the stairs.', None, None, 'Elevator', None, None , None, None, None, None, None)
 
+#PATH TO SECOND FLOOR
 Stairs1 = Building("Stairs", '', None, None, None, None, None,None, None, None, None, None)
 #SECOND FLOOR
 Weapon = Building('Weapon Room', '', None, None, None, None, 'Secret',None, None, None, None, None)
@@ -66,6 +68,10 @@ while True:
     print "Room: " + node.name
     print 
     print "Description: " + node.description 
+    
+    #WORD DEFINE
+    infected = "A person who had been contaminated by the gas"
+    zombie = 'A dead person risen from the dead.The chemicals within the gass had an effect on the dead makeing them come back to life'
     
     response = ['up', 'down', 'north', 'east', 'south', 'west', 'right', 'left', 'outside', 'inside'] 
     pick = ['pick up']
